6cea3a61c29d16059804f1b3a4ac6c368eb72d58
Amit Dixit <DixitAmit2@JohnDeere.com>
2021-09-25 22:34:08 +0530

    DisplayArrayUsingRecursion and FindMaximum of array

---
+++ src/com/dsa/array/DisplayArrayUsingRecursion.java
@@ -0,0 +1,34 @@
+package com.dsa.array;
+
+public class DisplayArrayUsingRecursion {
+
+    public static void main(String[] args)
+    {
+        int[] array={10,20,30,40,50};
+        displayArrayReverse(array,0);
+
+
+    }
+
+    //Ex =
+    public static void displayArray(int[] arry, int idx)
+    {
+        //Base condition
+        if(idx==arry.length)
+        return;
+        System.out.println(arry[idx]);
+        displayArray(arry,idx); // Recursive call
+
+    }
+
+    public static void displayArrayReverse(int[] arry, int idx)
+    {
+        //Base conditiondisplayArray1
+        if(idx==arry.length)
+            return;
+        displayArrayReverse(arry,idx+1); // Recursive call
+        System.out.println(arry[idx]);
+
+
+    }
+}
