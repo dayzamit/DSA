7492646c13263c82ae1313bcf2dcad65922f7e56
Amit Dixit <DixitAmit2@JohnDeere.com>
2021-09-28 15:56:19 +0530

    First occurrence of Target Number using non -improved or improved version

---
+++ src/com/dsa/array/FiocUsingImproveVersionRecursion.java
@@ -0,0 +1,26 @@
+package com.dsa.array;
+
+public class FiocUsingImproveVersionRecursion {
+
+    public static void main(String[] args) {
+        int[] arry = {2, 3, 6, 9, 8, 3, 2, 6, 2, 4};
+        System.out.println("first Occurence of Element" + findFirstOccrence(arry, 0, 3));
+
+    }
+
+    public static int findFirstOccrence(int[] arry, int idx, int targetData) {
+
+        if (idx == arry.length - 1)
+            return -1;
+
+        if (targetData == arry[idx])
+            return idx;
+        else {
+            int fiisa = findFirstOccrence(arry, idx + 1, targetData);
+
+            return fiisa;
+        }
+
+
+    }
+}
