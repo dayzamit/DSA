46ea207005f36635fa51a549d3d7dd466922676d
Amit Dixit <DixitAmit2@JohnDeere.com>
2021-09-30 13:12:47 +0530

    Find All indices of Target Number and display using Recursion

---
+++ src/com/dsa/array/AllIndicesUsingRecursion.java
@@ -0,0 +1,37 @@
+package com.dsa.array;
+
+public class AllIndicesUsingRecursion {
+
+
+    public static void main(String args[]) {
+        int[] arry = {2, 3, 6, 9, 8, 3, 2, 3, 6, 2, 4};
+        displayElemnt(findAllIndices(arry, 0, 3, 0), 0);
+    }
+
+    public static void displayElemnt(int[] arry, int idx) {
+
+        if (idx == arry.length)
+            return;
+        System.out.println("Element " + arry[idx]);
+        displayElemnt(arry, idx + 1);
+    }
+
+    public static int[] findAllIndices(int[] arry, int idx, int targetData, int fsf) {
+        //base case
+        if (idx == arry.length - 1) {
+            return new int[fsf];
+        }
+
+        if (arry[idx] == targetData) {
+            int[] iArry = findAllIndices(arry, idx + 1, targetData, fsf + 1);
+            iArry[fsf] = idx;
+            return iArry;
+
+        } else {
+            int[] iArry = findAllIndices(arry, idx + 1, targetData, fsf);
+            return iArry;
+
+        }
+    }
+
+}
